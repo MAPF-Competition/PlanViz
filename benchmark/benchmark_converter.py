87b73e9b34ea63a2430b1eff92ce3d09baa34c89
Shao-Hung Chan <shaohung@usc.edu>
2023-06-05 23:20:54 +0800

    Change input paths

--- benchmark/benchmark_converter.py
+++ benchmark/benchmark_converter.py
@@ -23,56 +23,53 @@ class BenchmarkConverter:
         self.start_loc:Dict[int, int] = {}     # Agent -> location
         self.tasks:List[int]  = []  # all the tasks
 
-        # Load the yaml file or the input arguments
+        # Load configuration file
         self.config: Dict = {}
-
-        if input_arg.config is not None:
+        if input_arg.config is not None:  # Load the yaml file
             config_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_arg.config)
             with open(config_dir, mode="r", encoding="utf-8") as fin:
                 self.config = yaml.load(fin, Loader=yaml.FullLoader)
-        else:
-            self.config["data_path"]:str = input_arg.data_path
-            self.config["map_name"]:str = input_arg.map_name
-            self.config["scen_name"]:str = input_arg.scen_name
-            self.config["num_of_agents"] = input_arg.num_of_agents
-
+        else:  #  Load the input arguments
+            self.config["map_path"]:str = input_arg.map_path
+            self.config["scen_path"]:List[str] = input_arg.scen_path
+            self.config["agent_num"] = input_arg.agent_num
             if input_arg.agent_file is not None:
                 self.config["agent_file"] = input_arg.agent_file
             if input_arg.task_file is not None:
                 self.config["task_file"]  = input_arg.task_file
-            if input_arg.num_of_tasks is not None:
-                self.config["num_of_tasks"] = input_arg.num_of_tasks
+            if input_arg.task_num is not None:
+                self.config["task_num"] = input_arg.task_num
+
+        assert self.config["agent_num"] is not None
+        assert self.config["map_path"] is not None
+        assert self.config["scen_path"] is not None
+        self.config["map_name"] = self.config["map_path"].split("/")[-1].split(".")[0]
+        self.config["scen_name"] = self.config["scen_path"][0].split("/")[-2].split("-")[-1]
+        assert self.config["scen_name"] in ["even", "random"]
 
         # Set the default values in self.config
-        assert self.config["num_of_agents"] is not None
-
-        if "num_of_tasks" not in self.config.keys():
-            self.config["num_of_tasks"] = -1  # put all goal locations into the task
-
+        if "task_num" not in self.config.keys():
+            self.config["task_num"] = -1  # put all goal locations into the task
         if "ins_id" not in self.config.keys():
             self.config["ins_id"]:List[int] = [i+1 for i in range(25)]  # from 1 to 25
-
         if "agent_file" not in self.config.keys():
             self.config["agent_file"] = "./" + self.config["map_name"] + "_" +\
-                self.config["scen_name"] + "_" + str(self.config["num_of_agents"]) + "_agents.txt"
+                self.config["scen_name"] + "_" + str(self.config["agent_num"]) + "_agents.txt"
         if "task_file" not in self.config.keys():
             self.config["task_file"] = "./" + self.config["map_name"] + "_" +\
-                self.config["scen_name"] + "_" + str(self.config["num_of_agents"]) + "_tasks.txt"
-
-        assert self.config["num_of_agents"] > 0
-        assert self.config["num_of_tasks"] > -2
+                self.config["scen_name"] + "_" + str(self.config["agent_num"]) + "_tasks.txt"
+        assert self.config["agent_num"] > 0
+        assert self.config["task_num"] > -2
+        assert len(self.config["scen_path"]) > 0
 
 
     def load_map_size(self):
-        map_fn = "mapf-map/" + self.config["map_name"] + ".map"
-        map_file = os.path.join(self.config["data_path"], map_fn)
-        print("Get the size of map " + self.config["map_name"])
-
-        if not os.path.exists(map_file):
+        print("Get the size of map " + self.config["map_path"])
+        if not os.path.exists(self.config["map_path"]):
             logging.error("\nNo map file is found!")
             sys.exit()
 
-        with open(map_file, mode="r", encoding="utf-8") as fin:
+        with open(self.config["map_path"], mode="r", encoding="utf-8") as fin:
             fin.readline()  # ignore "type"
             self.config["height"] = int(fin.readline().strip().split(' ')[1])
             self.config["width"]  = int(fin.readline().strip().split(' ')[1])
@@ -84,16 +81,12 @@ class BenchmarkConverter:
         assert "width"  in self.config.keys() and self.config["width"] > 0
 
         output_loc:List[Tuple[int,int]] = []
-        scen_dir = os.path.join(self.config["data_path"], "scen-" + self.config["scen_name"])
-        for _ins_idx_ in self.config["ins_id"]:
-            scen_fn = self.config["map_name"] + "-" + self.config["scen_name"] +\
-                "-" + str(_ins_idx_) + ".scen"
-            scen_file = os.path.join(scen_dir, scen_fn)
-            if not os.path.exists(scen_file):
+        for _scen_ in self.config["scen_path"]:
+            if not os.path.exists(_scen_):
                 logging.error("\nNo scen path is found!")
                 sys.exit()
 
-            with open(scen_file, mode="r", encoding="utf-8") as fin:
+            with open(_scen_, mode="r", encoding="utf-8") as fin:
                 head:str = fin.readline().rstrip("\n").split(" ")[0]  # ignore the first line
                 assert head == "version"  # we only process files from the MAPF suite
                 for line in fin.readlines():
@@ -106,10 +99,10 @@ class BenchmarkConverter:
 
 
     def generate_txt(self):
-        assert self.config["num_of_agents"] == len(self.start_loc)
+        assert self.config["agent_num"] == len(self.start_loc)
         with open(self.config["agent_file"], mode="w", encoding="utf-8") as fout:
-            fout.write(str(self.config["num_of_agents"]) + "\n")
-            for aid in range(self.config["num_of_agents"]):
+            fout.write(str(self.config["agent_num"]) + "\n")
+            for aid in range(self.config["agent_num"]):
                 fout.write(str(self.start_loc[aid]) + "\n")
 
         with open(self.config["task_file"], mode="w", encoding="utf-8") as fout:
@@ -124,25 +117,25 @@ class BenchmarkConverter:
         agent_tasks:Dict[int,List[int]] = {}
 
         # Generate start locations and the initial tasks, one for each agent
-        for aid in range(self.config["num_of_agents"]):
+        for aid in range(self.config["agent_num"]):
             cur_loc:Tuple[int,int] = all_locations.pop(0)
             self.start_loc[aid]:int = cur_loc[0]
             agent_tasks[aid] = [cur_loc[1]]
-        assert len(all_locations) + self.config["num_of_agents"] == num_all_locs
+        assert len(all_locations) + self.config["agent_num"] == num_all_locs
 
         # Generate other tasks from all_locations
         ag_cnt = 0
         for (sloc, gloc) in all_locations:
-            aid = ag_cnt % self.config["num_of_agents"]
+            aid = ag_cnt % self.config["agent_num"]
             agent_tasks[aid].append(sloc)
             agent_tasks[aid].append(gloc)
             ag_cnt += 1
 
         # Collect the tasks in a round robin order
-        has_task:List[bool] = [True for _ in range(self.config["num_of_agents"])]
+        has_task:List[bool] = [True for _ in range(self.config["agent_num"])]
         ag_cnt = 0
         while any(has_task):
-            aid = ag_cnt % self.config["num_of_agents"]
+            aid = ag_cnt % self.config["agent_num"]
             if agent_tasks[aid]:  # There is still elements in the list
                 self.tasks.append(agent_tasks[aid].pop(0))
             else:
@@ -150,18 +143,53 @@ class BenchmarkConverter:
             ag_cnt += 1
 
 
+    def verify(self):
+        debug_agent_num:int = -1
+        debug_tasks:List[int] = []
+        with open(self.config["agent_file"], mode="r", encoding="utf-8") as fin:
+            debug_agent_num = int(fin.readline().rstrip())
+            for _line_ in fin.readlines():
+                debug_tasks.append(int(_line_.rstrip()))
+        with open(self.config["task_file"], mode="r", encoding="utf-8") as fin:
+            fin.readline()  # ignore the number of tasks
+            for _line_ in fin.readlines():
+                debug_tasks.append(int(_line_.strip()))
+
+        debug_start = []
+        debug_goal  = []
+        for _tid_, _task_ in enumerate(debug_tasks):
+            if (_tid_ // self.config["agent_num"]) % 2 == 0:
+                debug_start.append(_task_)
+            else:
+                debug_goal.append(_task_)
+
+        for _scen_ in self.config["scen_path"]:
+            with open(_scen_, mode="r", encoding="utf-8") as fin:
+                fin.readline()
+                for _line_ in fin.readlines():
+                    _seg_ = _line_.split("\t")
+                    _start_:int = int(_seg_[5]) * self.config["width"] + int(_seg_[4])
+                    _goal_ :int = int(_seg_[7]) * self.config["width"] + int(_seg_[6])
+                    _dbs_ = debug_start.pop(0)
+                    _dbg_ = debug_goal.pop(0)
+                    assert _start_ == _dbs_
+                    assert _goal_  == _dbg_
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Take config.yaml as input!")
-    parser.add_argument("--config", type=str, default=None, help="configuration file")
-    parser.add_argument("--data_path", type=str, default="./", help="path to the MAPF benchmark")
-    parser.add_argument("--map_name", type=str, default="random-32-32-20", help="map name")
-    parser.add_argument("--scen_name", type=str, default="even", help="scen name")
-    parser.add_argument("--agent_file", type=str, default=None, help="output agent file")
-    parser.add_argument("--task_file", type=str, default=None, help="output task file")
-    parser.add_argument("--num_of_agents", type=int, default=None, help="number of agents")
-    parser.add_argument("--num_of_tasks", type=str, default=None, help="number of tasks")
+    parser.add_argument("--config", type=str, help="path to the configuration file", default=None)
+    parser.add_argument("--map", dest="map_path", type=str, help="path to the map file",
+                        default="~/mapf_benchmark/mapf-map/random-32-32-20.map")
+    parser.add_argument("--scen", dest="scen_path", type=str, help="path to all the scen files",
+                        nargs="*", default="~/mapf_benchmark/scen-even/random-32-32-20-even-1.scen")
+    parser.add_argument("--af", dest="agent_file", type=str, default=None, help="output agent file")
+    parser.add_argument("--tf", dest="task_file", type=str, default=None, help="output task file")
+    parser.add_argument("--na", dest="agent_num", type=int, default=None, help="number of agents")
+    parser.add_argument("--nt", dest="task_num", type=str, default=None, help="number of tasks")
     args = parser.parse_args()
 
     benchmark_converter = BenchmarkConverter(input_arg=args)
     benchmark_converter.convert_to_tasks()
     benchmark_converter.generate_txt()
+    benchmark_converter.verify()
